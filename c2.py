0b2133c909d6da6a2d8005e385449ba700128b4a
Christian Mazakas <christian.mazakas@gmail.com>
2025-04-22 14:38:56 -0700

    add initial prototype of c2.py

---
+++ c2.py
@@ -0,0 +1,238 @@
+#!/usr/bin/python3
+
+"""Python-based version of b2 using pure CMake"""
+
+import os
+import subprocess
+import dataclasses
+import argparse
+
+num_cores = os.cpu_count()
+
+command_mode: str | None = None
+
+@dataclasses.dataclass
+class BuildVariant:
+    """Represents a CMake-based build"""
+
+    address_model: str | None = None
+    toolset: str | None = None
+    variant: str | None = None
+    cxxstd: str | None = None
+
+def build_variant_to_build_dir(build_variant):
+    """Translates a build variant object into a named build directory to invoke CMake in"""
+
+    build_dir = "build"
+    if build_variant.toolset is not None:
+        build_dir += f"_{build_variant.toolset}"
+
+    if build_variant.cxxstd is not None:
+        build_dir += f"_{build_variant.cxxstd}"
+
+    if build_variant.variant is not None:
+        build_dir += f"_{build_variant.variant}"
+
+    addr = build_variant.address_model
+    if addr is not None:
+        if addr == '32':
+            build_dir += "_x86"
+        elif addr == '64':
+            build_dir += "_x64"
+
+    return os.path.join("build_c2y", build_dir)
+
+def toolset_to_cxx_compiler(toolset):
+    """Used to map toolsetes to something CMake can understand"""
+
+    if toolset.startswith("clang-"):
+        return toolset.replace("clang-", "clang++-")
+
+    if toolset.startswith("gcc-"):
+        return toolset.replace("gcc-", "g++-")
+
+    return None
+
+def variant_to_build_type(variant):
+    """Transforms a variant to something CMake understands"""
+
+    if variant == "debug":
+        return "Debug"
+
+    if variant == "release":
+        return "Release"
+
+    return None
+
+def build_variant_to_cmake_configure_args(build_variant: BuildVariant, build_dir: str):
+    """Programmatically generate the proper arguments to pass to CMake's configure phase"""
+
+    config_args = [
+        "cmake",
+        "-S", ".",
+        "-B", build_dir,
+        "-DBUILD_TESTING=ON",
+        "-DBOOST_INCLUDE_LIBRARIES=hash2",
+        "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON",
+    ]
+
+    if build_variant.toolset is not None:
+        cxx_compiler = toolset_to_cxx_compiler(build_variant.toolset)
+        config_args.append(f"-DCMAKE_CXX_COMPILER={cxx_compiler}")
+
+    if build_variant.variant is not None:
+        build_type = variant_to_build_type(build_variant.variant)
+        config_args.append(f"-DCMAKE_BUILD_TYPE={build_type}")
+
+    if build_variant.cxxstd is not None:
+        cxxstd = build_variant.cxxstd
+        config_args.append(f"-DCMAKE_CXX_STANDARD={cxxstd}")
+
+    if build_variant.address_model == '32':
+        config_args.append("-DCMAKE_CXX_FLAGS_INIT=\"-m32\"")
+
+    return config_args
+
+
+def build_variant_to_cmake_build_args(build_variant: BuildVariant, build_dir: str):
+    """Programmatically generate the proper arguments to pass to CMake's build phase"""
+
+    build_args = [
+        "cmake",
+        "--build", build_dir,
+        "--target", "tests",
+        f"-j{str(num_cores)}",
+    ]
+
+    if build_variant.variant is not None:
+        build_type = variant_to_build_type(build_variant.variant)
+        build_args.append(f"--config {build_type}")
+
+    return build_args
+
+def build_boost(build_variant):
+    """"Build a specific Boost configuration"""
+
+    build_dir = build_variant_to_build_dir(build_variant)
+
+    print("configuring Boost")
+    config_args = build_variant_to_cmake_configure_args(build_variant, build_dir)
+    subprocess.run(config_args, check=True)
+
+
+    print("building Boost")
+    build_args = build_variant_to_cmake_build_args(build_variant, build_dir)
+    subprocess.run(build_args, check=True)
+
+    if command_mode == "test":
+        subprocess.run(["ctest", "--test-dir", build_dir], check=True)
+
+def parse_args():
+    """Parse CLI args and form the build variants array"""
+
+    parser = argparse.ArgumentParser(description="Parse all build options and assemble the matrix.")
+
+    parser.add_argument(
+        "command",
+        type=str,
+        help="Main driver command. Either just `build` or `test`. `test` implies `build` but also "
+             "invokes `ctest` for each generated build directory."
+    )
+
+    parser.add_argument(
+        "--cxxstd",
+        type=str,
+        help="A comma-separated list of C++ standard versions (e.g., 'cxxstd=11,17,20')."
+    )
+
+    parser.add_argument(
+        "--toolset",
+        type=str,
+        help="A comma-separated list of C++ toolchains to use (e.g. toolset=gcc-14,clang-19 "
+             "[no '++' required])"
+    )
+
+    parser.add_argument(
+        "--variant",
+        type=str,
+        help="A comma-separated list of C++ build types (e.g variant=debug,release or "
+             "variant=release)"
+    )
+
+    parser.add_argument(
+        "--address-model",
+        type=str,
+        help="A comma-separated list of architectures (e.g. address-model=32,64)",
+        dest="address_model"
+    )
+
+    args = parser.parse_args()
+
+    cxxstds = []
+    toolsets = []
+    variants = []
+    address_models = []
+
+    if args.command:
+        command = args.command
+        if command not in ('build', 'test'):
+            raise ValueError("The only permitted sub-commands for "
+                             "c2.py are: \"build\" or \"test\".")
+        global command_mode
+        command_mode = command
+
+    if args.cxxstd:
+        result = args.cxxstd.split(",")
+        cxxstds += result
+    else:
+        print("Invalid input. Make sure to use the form `--cxxstd 11,20`.")
+
+    if args.toolset:
+        result = args.toolset.split(",")
+        toolsets += result
+    else:
+        print("Invalid input. Make sure to use the form `--toolset gcc-14,clang-19`.")
+
+    if args.variant:
+        result = args.variant.split(",")
+        variants += result
+    else:
+        print("Invalid input. Make sure to use the form `--variant debug,release`.")
+
+    if args.address_model:
+        result = args.address_model.split(",")
+        address_models += result
+    else:
+        print("Invalid input. Make sure to use the form `--address-model 32,64`.")
+
+    print(f"read in the following cxxstds: {cxxstds}")
+    print(f"read in the following toolsets: {toolsets}")
+    print(f"read in the following variants: {variants}")
+    print(f"read in the following address-models: {address_models}")
+
+    build_variants = []
+    for cxxstd in cxxstds:
+        for toolset in toolsets:
+            for variant in variants:
+                for addr in address_models:
+                    build_variants.append(
+                        BuildVariant(
+                            toolset=toolset,
+                            variant=variant,
+                            cxxstd=cxxstd,
+                            address_model=addr
+                        )
+                    )
+
+    return build_variants
+
+def init():
+    """Main entry for bulk-building Boost via CMake"""
+
+    print("starting c2.py script")
+    build_variants = parse_args()
+    for build_variant in build_variants:
+        build_boost(build_variant)
+
+if __name__ == "__main__":
+    init()
