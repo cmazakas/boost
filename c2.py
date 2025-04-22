f223b4933c4403a139c9f2a430f54bdbe553d3b3
Christian Mazakas <christian.mazakas@gmail.com>
2025-04-22 15:57:53 -0700

    update script to support building specified libraries

--- c2.py
+++ c2.py
@@ -10,6 +10,9 @@ import argparse
 num_cores = os.cpu_count()
 
 command_mode: str | None = None
+cmake_generator: str | None = None
+num_jobs: int | None = None
+library: str | None = None
 
 @dataclasses.dataclass
 class BuildVariant:
@@ -20,15 +23,15 @@ class BuildVariant:
     variant: str | None = None
     cxxstd: str | None = None
 
-def build_variant_to_build_dir(build_variant):
+def build_variant_to_build_dir(build_variant: BuildVariant):
     """Translates a build variant object into a named build directory to invoke CMake in"""
 
-    build_dir = "build"
+    build_dir = f"build_{library}"
     if build_variant.toolset is not None:
         build_dir += f"_{build_variant.toolset}"
 
     if build_variant.cxxstd is not None:
-        build_dir += f"_{build_variant.cxxstd}"
+        build_dir += f"_std{build_variant.cxxstd}"
 
     if build_variant.variant is not None:
         build_dir += f"_{build_variant.variant}"
@@ -40,7 +43,7 @@ def build_variant_to_build_dir(build_variant):
         elif addr == '64':
             build_dir += "_x64"
 
-    return os.path.join("build_c2y", build_dir)
+    return os.path.join("build_c2py", build_dir)
 
 def toolset_to_cxx_compiler(toolset):
     """Used to map toolsetes to something CMake can understand"""
@@ -72,12 +75,16 @@ def build_variant_to_cmake_configure_args(build_variant: BuildVariant, build_dir
         "-S", ".",
         "-B", build_dir,
         "-DBUILD_TESTING=ON",
-        "-DBOOST_INCLUDE_LIBRARIES=hash2",
+        f"-DBOOST_INCLUDE_LIBRARIES={library}",
         "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON",
     ]
 
+    if cmake_generator:
+        config_args += ["-G", cmake_generator]
+
     if build_variant.toolset is not None:
         cxx_compiler = toolset_to_cxx_compiler(build_variant.toolset)
+        config_args.append(f"-DCMAKE_C_COMPILER={build_variant.toolset}")
         config_args.append(f"-DCMAKE_CXX_COMPILER={cxx_compiler}")
 
     if build_variant.variant is not None:
@@ -101,8 +108,10 @@ def build_variant_to_cmake_build_args(build_variant: BuildVariant, build_dir: st
         "cmake",
         "--build", build_dir,
         "--target", "tests",
-        f"-j{str(num_cores)}",
     ]
+
+    if num_jobs:
+        build_args.append(f"-j{num_jobs}")
 
     if build_variant.variant is not None:
         build_type = variant_to_build_type(build_variant.variant)
@@ -110,7 +119,7 @@ def build_variant_to_cmake_build_args(build_variant: BuildVariant, build_dir: st
 
     return build_args
 
-def build_boost(build_variant):
+def build_boost(build_variant: BuildVariant):
     """"Build a specific Boost configuration"""
 
     build_dir = build_variant_to_build_dir(build_variant)
@@ -133,10 +142,24 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Parse all build options and assemble the matrix.")
 
     parser.add_argument(
+        "library",
+        type=str,
+        help="Name of the library to build tests for."
+    )
+
+    parser.add_argument(
         "command",
         type=str,
         help="Main driver command. Either just `build` or `test`. `test` implies `build` but also "
              "invokes `ctest` for each generated build directory."
+    )
+
+    parser.add_argument("-G", type=str, help="The CMake generator to use.", dest="generator")
+
+    parser.add_argument(
+        "-j", type=int,
+        dest="jobs",
+        help="Number of jobs used to build with CMake.",
     )
 
     parser.add_argument(
@@ -180,30 +203,46 @@ def parse_args():
                              "c2.py are: \"build\" or \"test\".")
         global command_mode
         command_mode = command
+    else:
+        raise ValueError("Must specify a build command such as `build` or `test.")
+
+    if args.library:
+        global library
+        library = args.library
+    else:
+        raise ValueError("Must specify a library to build, such as `hash2` or `unordered`.")
+
+    if args.generator:
+        global cmake_generator
+        cmake_generator = args.generator
+
+    if args.jobs:
+        global num_jobs
+        num_jobs = args.jobs
 
     if args.cxxstd:
         result = args.cxxstd.split(",")
         cxxstds += result
     else:
-        print("Invalid input. Make sure to use the form `--cxxstd 11,20`.")
+        cxxstds.append(None)
 
     if args.toolset:
         result = args.toolset.split(",")
         toolsets += result
     else:
-        print("Invalid input. Make sure to use the form `--toolset gcc-14,clang-19`.")
+        toolsets.append(None)
 
     if args.variant:
         result = args.variant.split(",")
         variants += result
     else:
-        print("Invalid input. Make sure to use the form `--variant debug,release`.")
+        variants.append(None)
 
     if args.address_model:
         result = args.address_model.split(",")
         address_models += result
     else:
-        print("Invalid input. Make sure to use the form `--address-model 32,64`.")
+        address_models.append(None)
 
     print(f"read in the following cxxstds: {cxxstds}")
     print(f"read in the following toolsets: {toolsets}")
